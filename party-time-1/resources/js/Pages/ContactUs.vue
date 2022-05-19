e0059ad21682134bbbf9feae00666e43b888dd04
YuhaoZhang515 <70833541+YuhaoZhang515@users.noreply.github.com>
2022-05-19 23:40:49 +0930

    1

---
+++ party-time-1/resources/js/Pages/ContactUs.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="min-h-screen bg-gray-100">
+      <nav class="bg-white border-b border-gray-100"  >
+        <!-- Primary Navigation Menu -->
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="flex justify-between h-20">
+            <div class="flex">
+              <!-- Logo -->
+              <div class="flex-shrink-0 flex items-left">
+                <Link :href="route('gusers.lending_page')">
+                  <BreezeApplicationLogo class="block h-20 w-auto" />
+                </Link>
+              </div>
+               <div class="flex items-center ml-6">
+                <BreezeDropdownLink :href="route('gusers.lending_page')">
+                  Home
+                </BreezeDropdownLink>
+              </div>
+               <div class="flex items-center ml-6">
+                <BreezeDropdownLink :href="route('gusers.login')">
+                  Login
+                </BreezeDropdownLink>
+              </div>
+               <div class="flex items-center ml-6">
+                <BreezeDropdownLink :href="route('gusers.register')">
+                  Register
+                </BreezeDropdownLink>
+              </div>
+               <div class="flex items-center ml-6">
+                <BreezeDropdownLink :href="route('guser.aboutus')">
+                  About Us
+                </BreezeDropdownLink>
+              </div>
+               <div class="flex items-center ml-6">
+                <BreezeDropdownLink :href="route('guser.contactus')">
+                  Contact Us
+                </BreezeDropdownLink>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+
+      </nav>
+
+      <!-- Page Content -->
+      <main class="min-w-full">
+        <h1 class="text-center py-4 text-6xl font-bold">Contact Information</h1>
+        <h1 class="text-center py-4 text-3xl font-bold">Give us a call: 000000000</h1>
+        <h1 class="text-center py-4 text-3xl font-bold">Send us an email: sample-email@gmail.com</h1>
+       
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.3396017276827!2d138.60038551534217!3d-34.923018980378075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ab0ced418957257%3A0x34877b9b34a650ac!2sApple%20Rundle%20Place!5e0!3m2!1sen!2sau!4v1652365617569!5m2!1sen!2sau" width="1900" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </main>
+    </div>
+  </div>
+  <Footer />
+</template>
+
+<script>
+import BreezeApplicationLogo from "@/Components/ApplicationLogo.vue";
+import BreezeDropdown from "@/Components/Dropdown.vue";
+import BreezeDropdownLink from "@/Components/DropdownLink.vue";
+import BreezeNavLink from "@/Components/NavLink.vue";
+import BreezeResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Link } from "@inertiajs/inertia-vue3";
+import Footer from '@/Components/FooterUser.vue'
+
+export default {
+  components: {
+    BreezeApplicationLogo,
+    BreezeDropdown,
+    BreezeDropdownLink,
+    BreezeNavLink,
+    BreezeResponsiveNavLink,
+    Link,
+    Footer
+  },
+  props: {
+    isLoggedIn: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+};
+</script>
---
+++ party-time-1/resources/js/Pages/SuperAdmin/Staff/StaffAdd.vue
@@ -0,0 +1,172 @@
+<template>
+    <BreezeAuthenticatedLayout>
+        <h1 class="text-center py-12 text-3xl font-bold">Staff Add</h1>
+        <div class="flex justify-center">
+            <form
+                @submit.prevent="submitForm"
+                class="md:w-1/3 text-center w-full"
+            >
+                <BreezeInput
+                    id="full_name"
+                    type="text"
+                    class="mt-2 block w-full p-2"
+                    placeholder="Full Name"
+                    v-model="formData.full_name"
+                    required
+                />
+                <BreezeInput
+                    id="email"
+                    type="email"
+                    class="mt-2 block w-full p-2"
+                    placeholder="Email Address"
+                    v-model="formData.email"
+                    required
+                />
+                <BreezeInput
+                    id="phone"
+                    type="phone"
+                    class="mt-2 block w-full p-2"
+                    placeholder="Phone Number"
+                    v-model="formData.phone"
+                    required
+                />
+
+                <!-- <BreezeInput
+          id="password"
+          type="password"
+          class="mt-2 block w-full p-2"
+          placeholder="Enter New Password"
+          v-model="formData.password"
+          required
+        /> -->
+
+                <div class="relative w-full">
+                    <div
+                        class="absolute inset-y-0 right-0 flex items-center px-2"
+                    >
+                        <input
+                            class="hidden js-password-toggle"
+                            id="toggle"
+                            type="checkbox"
+                        />
+                        <label
+                            class="mt-2 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label"
+                            for="toggle"
+                            @click="togglePassword = !togglePassword"
+                        >
+                            <svg
+                                version="1.1"
+                                id="Capa_1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink"
+                                x="0px"
+                                y="0px"
+                                width="20.04px"
+                                height="20.04px"
+                                viewBox="0 0 442.04 442.04"
+                                style="enable-background: new 0 0 442.04 442.04"
+                                xml:space="preserve"
+                            >
+                                <g>
+                                    <g>
+                                        <path
+                                            d="M221.02,341.304c-49.708,0-103.206-19.44-154.71-56.22C27.808,257.59,4.044,230.351,3.051,229.203
+			c-4.068-4.697-4.068-11.669,0-16.367c0.993-1.146,24.756-28.387,63.259-55.881c51.505-36.777,105.003-56.219,154.71-56.219
+			c49.708,0,103.207,19.441,154.71,56.219c38.502,27.494,62.266,54.734,63.259,55.881c4.068,4.697,4.068,11.669,0,16.367
+			c-0.993,1.146-24.756,28.387-63.259,55.881C324.227,321.863,270.729,341.304,221.02,341.304z M29.638,221.021
+			c9.61,9.799,27.747,27.03,51.694,44.071c32.83,23.361,83.714,51.212,139.688,51.212s106.859-27.851,139.688-51.212
+			c23.944-17.038,42.082-34.271,51.694-44.071c-9.609-9.799-27.747-27.03-51.694-44.071
+			c-32.829-23.362-83.714-51.212-139.688-51.212s-106.858,27.85-139.688,51.212C57.388,193.988,39.25,211.219,29.638,221.021z"
+                                        />
+                                    </g>
+                                    <g>
+                                        <path
+                                            d="M221.02,298.521c-42.734,0-77.5-34.767-77.5-77.5c0-42.733,34.766-77.5,77.5-77.5c18.794,0,36.924,6.814,51.048,19.188
+			c5.193,4.549,5.715,12.446,1.166,17.639c-4.549,5.193-12.447,5.714-17.639,1.166c-9.564-8.379-21.844-12.993-34.576-12.993
+			c-28.949,0-52.5,23.552-52.5,52.5s23.551,52.5,52.5,52.5c28.95,0,52.5-23.552,52.5-52.5c0-6.903,5.597-12.5,12.5-12.5
+			s12.5,5.597,12.5,12.5C298.521,263.754,263.754,298.521,221.02,298.521z"
+                                        />
+                                    </g>
+                                    <g>
+                                        <path
+                                            d="M221.02,246.021c-13.785,0-25-11.215-25-25s11.215-25,25-25c13.786,0,25,11.215,25,25S234.806,246.021,221.02,246.021z"
+                                        />
+                                    </g>
+                                </g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                            </svg>
+                        </label>
+                    </div>
+                    <input
+                        class="w-full border-2 shadow mt-2 p-2 text-black border-gray-200"
+                        id="password"
+                        :type="togglePassword ? 'text' : 'password'"
+                        placeholder="Password"
+                        v-model="formData.password"
+                        autocomplete="off"
+                    />
+                </div>
+
+                <button
+                    class="rounded-lg mt-6 py-2 px-4 bg-green-700 text-white"
+                >
+                    Add Staff
+                </button>
+            </form>
+        </div>
+        <Footer />
+    </BreezeAuthenticatedLayout>
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import { Link } from "@inertiajs/inertia-vue3";
+import BreezeInput from "@/Components/Input.vue";
+import axios from "axios";
+import Footer from "@/Components/Footer.vue";
+
+export default {
+    components: {
+        BreezeAuthenticatedLayout,
+        Link,
+        BreezeInput,
+        Footer,
+    },
+
+    data() {
+        return {
+            formData: {
+                full_name: "",
+                email: "",
+                phone: "",
+                password: "",
+            },
+            togglePassword: false,
+        };
+    },
+
+    methods: {
+        submitForm() {
+            axios.post("/superadmin/staff/add", this.formData).then((res) => {
+                window.location = "/superadmin/staff";
+            });
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
